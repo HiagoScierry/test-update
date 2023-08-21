78589db33d33c25a0973b4779349f0d63cf7471f
Hiago Moreira <47890178+HiagoScierry@users.noreply.github.com>
2023-08-20 22:22:37 -0300

    feat: wip create migration to database

---
+++ database/migrations/2023_08_21_010618_create_client_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name', 100)->nullable(false);
+            $table->string('document', 14)->nullable(false);
+            $table->date('birth_date')->nullable(false);
+            $table->string('sex', 1)->nullable(false);
+            $table->string('address', 100)->nullable(false);
+            $table->string('state', 2)->nullable(false);
+            $table->string('city', 100)->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client');
+    }
+};
