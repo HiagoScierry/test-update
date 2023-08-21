38f2e934bf51922dd39a1135bfb79a6e7bc80106
Hiago Moreira <47890178+HiagoScierry@users.noreply.github.com>
2023-08-21 11:31:06 -0300

    update view

--- resources/views/listClients.blade.php
+++ resources/views/listClients.blade.php
@@ -4,7 +4,7 @@
 
     <div class="mx-4 mt-5 p-5 bg-slate-500 rounded-lg shadow-lg">
         <h2 class="text-xl font-semibold mb-3 text-gray-700">Filtrar Clientes</h2>
-        <form method="GET">
+        <div>
             <div class="flex items-center justify-between gap-10">
                 <div class="w-full">
                     <label for="nome" class="block text-sm font-medium text-gray-700">Nome do Cliente:</label>
@@ -20,7 +20,7 @@
                 </div>
                 <div class="w-full">
                     <label for="sexo" class="block text-sm font-medium text-gray-700">Sexo:</label>
-                    <select name="sexo" id="sexo" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                    <select name="sexo" id="sexoFilter" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         <option value=""></option>
                         <option value="M">Masculino</option>
                         <option value="F">Feminino</option>
@@ -30,7 +30,7 @@
             <div class="flex items-center justify-between gap-10 mt-4">
 
                 <div class="w-full">
-                    <select aria-placeholder="Estado" name="uf" id="ufFilter" class="mt-1 p-2 block w-full text-slate-600 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" onchange="populateCities()">
+                    <select aria-placeholder="Estado" name="uf" id="ufFilter" class="mt-1 p-2 block w-full text-slate-600 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" onchange="populateCities(0)">
                         <option value="" disabled selected>Estado</option>
                     </select>
                 </div>
@@ -43,7 +43,7 @@
 
                 <button onclick="filterData()" class="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50">Filtrar</button>
             </div>
-        </form>
+        </div>
     </div>
 
 
@@ -74,7 +74,8 @@
                         <td class="border px-4 py-2">{{ $client->sex }}</td>
                         <td class="border px-4 py-2">{{ $client->state }}</td>
                         <td class="border px-4 py-2">{{ $client->city }}</td>
-                        <td class="border px-4 py-2">
+                        <td class="border px-4 py-2 flex justify-between gap-2">
+                            <button onclick="getDataFromID('{{$client->id}}')" class="bg-orange-600 text-white px-4 py-2 rounded-md hover:bg-orange-700 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50">Editar</button>
                             <button onclick="deleteClient('{{$client->id}}')" class="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50">Excluir</button>
                         </td>
                     </tr>
@@ -93,23 +94,23 @@
             <div class="modal-container w-full md:max-w-5xl mx-auto rounded shadow-lg z-50 overflow-y-auto">
                 <div class="mx-4 mt-5 p-5 bg-slate-500 rounded-lg shadow-lg">
                     <h2 class="text-xl font-semibold mb-3 text-gray-700">Criar Clientes</h2>
-                    <form method="GET">
+                    <div>
                         <div class="flex items-center justify-between gap-10">
                             <div class="w-full">
                                 <label for="nome" class="block text-sm font-medium text-gray-700">Nome do Cliente:</label>
-                                <input type="text" name="nome" id="nome" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <input type="text" name="nome" id="nomeForm" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             </div>
                             <div class="w-full">
                                 <label for="cpf" class="block text-sm font-medium text-gray-700">CPF:</label>
-                                <input type="text" name="cpf" id="cpf" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <input type="text" name="cpf" id="cpfForm" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             </div>
                             <div class="w-full">
                                 <label for="data_nascimento" class="block text-sm font-medium text-gray-700">Data de Nascimento:</label>
-                                <input type="date" name="data_nascimento" id="data_nascimento" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <input type="date" name="data_nascimento" id="data_nascimentoForm" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             </div>
                             <div class="w-full">
                                 <label for="sexo" class="block text-sm font-medium text-gray-700">Sexo:</label>
-                                <select name="sexo" id="sexo" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <select name="sexo" id="sexoForm" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                     <option value="M">Masculino</option>
                                     <option value="F">Feminino</option>
                                 </select>
@@ -117,24 +118,26 @@
                         </div>
                         <div class="flex items-center justify-between gap-10 mt-4">
                             <div class="w-full">
-                                <input type="text" placeholder="Endereço" name="endereco" id="endereco" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <input type="text" placeholder="Endereço" name="endereco" id="enderecoForm" class="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             </div>
 
                             <div class="w-full">
-                                <select aria-placeholder="Estado" name="uf" id="uf" class="mt-1 p-2 block w-full text-slate-600 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" onchange="populateCities()">
+                                <select aria-placeholder="Estado" name="uf" id="ufForm" class="mt-1 p-2 block w-full text-slate-600 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" onchange="populateCities(1)">
                                     <option value="" disabled selected>Estado</option>
                                 </select>
                             </div>
                             <div class="w-full">
-                                <select name="city" id="city" class="mt-1 p-2 block w-full text-slate-600 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <select name="city" id="cityForm" class="mt-1 p-2 block w-full text-slate-600 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                     <option value="" disabled selected>Cidade</option>
                                 </select>
                             </div>
 
+                            <input type="hidden" id="editID" value="">
 
-                            <button" class="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50">Salvar</button>
+                            <button onclick="editClient()" id="editClient" class="hidden bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50">Editar</button>
+                            <button onclick="createClient()" id="createClient" class="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50">Salvar</button>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -183,20 +186,22 @@
 
 
     function populateUfs() {
-        const ufSelect = document.getElementById("ufFilter");
+        const ufSelect = document.querySelectorAll("[name=uf]");
 
         statesAndCities.forEach(state => {
-            ufSelect.innerHTML += `<option value="${state.uf}">${state.state}</option>`
+            ufSelect.forEach(select => {
+                select.innerHTML += `<option value="${state.uf}">${state.state}</option>`
+            })
         })
     }
 
-    function populateCities() {
-        const stateInput = document.getElementById("ufFilter");
-        const citySelect = document.getElementById("city")
+    function populateCities(indexFilter) {
+        const stateInput = document.querySelectorAll("[name=uf]")[indexFilter];
+        const citySelect = document.querySelectorAll("[name=city]")[indexFilter];
 
         const selectedIndex = stateInput.selectedIndex;
 
-        const selectedState = statesAndCities[selectedIndex]
+        const selectedState = statesAndCities[selectedIndex - 1]
 
         citySelect.innerHTML = ""
         selectedState.cities.forEach(city => {
@@ -226,10 +231,18 @@
 
 
     //Adicionando clientes
-    function openModal() {
+    function openModal(isEditable = false) {
         const modal = document.getElementById("Modal-add")
         modal.classList.remove("hidden")
         modal.classList.add("block")
+
+
+        if (!isEditable) {
+            document.getElementById("createClient").classList.remove("hidden")
+            document.getElementById("editClient").classList.add("hidden")
+        }
+
+
     }
 
     function closeModal() {
@@ -242,59 +255,132 @@
     outOfModal.addEventListener("click", closeModal)
 
 
+    async function createClient() {
+        event.preventDefault()
+
+        const name = document.getElementById("nomeForm").value
+        const cpf = document.getElementById("cpfForm").value
+        const nascimento = document.getElementById("data_nascimentoForm").value
+        const sexo = document.getElementById("sexoForm").value
+        const uf = document.getElementById("ufForm").value
+        const city = document.getElementById("cityForm").value
+        const endereco = document.getElementById("enderecoForm").value
+
+        if (!name || !cpf || !nascimento || !sexo || !uf || !city) {
+            alert("Preencha todos campos criar um cliente")
+            return
+        }
+
+        await fetch(`http://localhost:8000/api/clients/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    name,
+                    birth_date: nascimento,
+                    document: cpf,
+                    sex: sexo,
+                    address: endereco,
+                    state: uf,
+                    city
+                })
+            }).then(response => response.json())
+            .catch(err => alert(err.message))
+
+        window.location.reload()
+
+    }
+
 
     //Filtrando clientes
-   function filterData(){
-    const name = document.getElementById("nomeFilter").textContent
-    const cpf = document.getElementById("cpfFilter").textContent
-    const nascimento = document.getElementById("data_nascimentoFilter").textContent
-    const sexo = document.getElementById("sexoFilter");
-    const uf = document.getElementById("ufFilter").value
-    const city = document.getElementById("cityFilter").value
+    function filterData() {
 
-    console.log(sexo.options[sexo.selectedIndex].text);
+        const name = document.getElementById("nomeFilter").value
+        const cpf = document.getElementById("cpfFilter").value
+        const nascimento = document.getElementById("data_nascimentoFilter").value
+        const sexo = document.getElementById("sexoFilter").value
+        const uf = document.getElementById("ufFilter").value
+        const city = document.getElementById("cityFilter").value
 
-    if(name == "" && cpf == "" && nascimento == "" && sexo == "" && uf == "" && city == "") {
-        alert("Preencha pelo menos um campo para filtrar");
-        return;
-    }
+        if (!name && !cpf && !nascimento && !sexo && !uf && !city) {
+            alert("Preencha pelo menos um campo para filtrar")
+            return
+        }
+        window.location.href = `http://localhost:8000/?name=${name}&document=${cpf}&birth_date=${nascimento}&sex=${sexo}&state=${uf}&city=${city}`
 
-    let filters = ""
 
-    if(name != "") {
-        filters += `name=${name}&`
-    }
-
-    if(cpf != "") {
-        filters += `document=${cpf}&`
-    }
-
-    if(nascimento != "") {
-        filters += `birth_date=${nascimento}&`
-    }
-
-    if(sexo.options[sexo.selectedIndex].text != "") {
-        filters += `sex=${sexo}&`
-    }
-
-    if(uf != "") {
-        filters += `state=${uf}&`
-    }
-
-    if(city != "") {
-        filters += `city=${city}&`
     }
 
 
 
+    //Editar clientes
 
-    location.href = `http://localhost:8000/api/clients/?${filters}`
+    async function getDataFromID(id) {
+        event.preventDefault()
+
+        const response = await fetch(`http://localhost:8000/api/clients/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const data = await response.json()
+
+        document.getElementById("editID").value = id
+        document.getElementById("nomeForm").value = data.name
+        document.getElementById("cpfForm").value = data.document
+        document.getElementById("data_nascimentoForm").value = data.birth_date
+        document.getElementById("sexoForm").value = data.sex
+        document.getElementById("ufForm").value = data.state
+        document.getElementById("cityForm").value = data.city
+        document.getElementById("enderecoForm").value = data.address
+
+        document.getElementById("createClient").classList.add("hidden")
+        document.getElementById("editClient").classList.remove("hidden")
+
+        openModal(true)
+
+    }
+
+    async function editClient(id) {
+        const idEdit = document.getElementById("editID").value
+        const name = document.getElementById("nomeForm").value
+        const cpf = document.getElementById("cpfForm").value
+        const nascimento = document.getElementById("data_nascimentoForm").value
+        const sexo = document.getElementById("sexoForm").value
+        const uf = document.getElementById("ufForm").value
+        const city = document.getElementById("cityForm").value
+        const endereco = document.getElementById("enderecoForm").value
+
+        if (!name || !cpf || !nascimento || !sexo || !uf || !city) {
+            alert("Preencha todos campos para editar um cliente")
+            return
+        }
+
+        fetch(`http://localhost:8000/api/clients/${idEdit}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                birth_date: nascimento,
+                document: cpf,
+                sex: sexo,
+                address: endereco,
+                state: uf,
+                city
+            })
+        }).then(response => response.json())
+
+        window.location.reload()
 
 
-   }
-
-
-
+    }
 </script>
 
 
